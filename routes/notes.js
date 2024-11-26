60abb937159a6b9936ce6a5deed38bf2f44ce653
yuukuma <coffeekumasan@gmail.com>
2024-11-27 02:52:10 +0900

    add yesno api

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "************************";
+const uri = "*********************************";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
